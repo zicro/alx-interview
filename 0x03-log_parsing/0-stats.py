d82c9f05495df3d47b819e9f1c320d0157e4b2d5
xLion <larhnimikit@gmail.com>
2024-03-26 11:15:24 +0000

    re-update data

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,39 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""
+Log parsing
+"""
+
 import sys
 
+if __name__ == '__main__':
 
-def print_metrics(total_file_size, status_code_counts):
-    """Print the metrics"""
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_code_counts):
-        count = status_code_counts[code]
-        if count > 0:
-            print('{}: {}'.format(code, count))
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
-total_file_size = 0
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
-}
-
-line_count = 0
-
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-        try:
-            status_code = int(parts[-2])
-            status_code_counts[status_code] += 1
-        except Exception as err:
-            pass
-
-        try:
-            file_size = int(parts[-1])
-            total_file_size += file_size
-        except Exception as err:
-            pass
-
-        if line_count % 10 == 0:
-            print_metrics(total_file_size, status_code_counts)
-finally:
-    print_metrics(total_file_size, status_code_counts)
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
