57a80a089d5b79d5addcaaed43a2c1dca7cd4a89
xLion <larhnimikit@gmail.com>
2024-04-22 10:58:40 +0100

    the matrix rotated..

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix """
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = tmp
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+rotate the matrix ..
