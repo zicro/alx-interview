1e161de342ed8482fc9856ef0dd2bb1b81e13501
Omae <larhnimikit@gmail.com>
2024-04-30 04:25:36 -0700

    acheive tasks

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Making change"""
+
+
+def makeChange(coins, total):
+    """making change"""
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
